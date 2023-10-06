d3cd2bd2957c481b0e7601db8094337c846e087d
Terry Wambui <shikuwambui@gmail.com>
2023-10-06 13:39:33 +0300

    API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+(document).ready(function () {
     // Create an empty object to store amenities with their status (checked or unchecked)
     const amenities = {};
 
@@ -28,5 +28,24 @@ $(document).ready(function () {
 
         // Update the text inside the <h4> tag within the 'div.amenities' with the updated list of selected amenities
         $('div.amenities h4').text(amenitiesList);
+    });
+});
+
+
+
+// Wait for the document to be fully loaded
+$('document').ready(function () {
+    // Define the URL of the API endpoint
+    const url = 'http://0.0.0.0:5001/api/v1/status/';
+
+    // Send a GET request to the API endpoint
+    $.get(url, function (data) {
+        if (data.status === 'OK') {
+            // If the status is "OK," add the class "available" to the element with id "api_status"
+            $('#api_status').addClass('available');
+        } else {
+            // If the status is not "OK," remove the class "available" from the element with id "api_status"
+            $('#api_status').removeClass('available');
+        }
     });
 });
